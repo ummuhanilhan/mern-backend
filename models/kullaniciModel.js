6738289b584f717596d7757bb6baad8a0040adb2
ummuhanilhan <ummuhan.ilhan@bil.omu.edu.tr>
2022-10-05 16:21:52 +0300

    parola şifreleme işlemi

--- models/kullaniciModel.js
+++ models/kullaniciModel.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
+const bcrypt=require('bcrypt')
 
 const kullaniciSema=new Sema({
     email:{
@@ -12,5 +13,21 @@ const kullaniciSema=new Sema({
         required:true
     }
 })
+
+kullaniciSema.statics.signup=async function(email,parola){
+    const kontrolKullanici=await this.findOne({email}) // kullanıcının var olup olmadığını kontrol ediyoruz
+    if(kontrolKullanici){
+        throw new Error('Bu email ile kayıtlı kullanıcı var')
+    }
+    const salt=await bcrypt.genSalt(10)
+
+    const sifrelenmisParola=await bcrypt.hash(parola,salt)
+
+    const kullanici=await this.create({
+        email,
+        parola:sifrelenmisParola
+    })
+    return kullanici
+}
 
 module.exports=mongoose.model('Kullanıcı',kullaniciSema)
